78b78e589055e568450b0e98673b431a5aa9fdc8
David Winchester <dwinchester@salesforce.com>
2020-12-31 20:20:34 +0100

    Removed unit tests for brevity

--- tests/test_points.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.points import Point
-
-
-class TestPoint(unittest.TestCase):
-    def test_coordinate_values(self):
-        point = Point(50, 100)
-        self.assertEqual(point.x, 50)
-        self.assertEqual(point.y, 100)
